89723272256fb2baf1bae447d007df23da584088
Jamie Sykes <jaymehsykes@gmail.com>
2019-02-27 23:32:02 +0000

    Demonstrates the other models.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import codebase from 'codebase-npm';
+import codebase, { TimeSession, Project, Ticket, User } from 'codebase-npm';
 
 let codebaseConnection = new codebase('', '', '');
+let project = new Project(1, 'project', 'active', '/project', 1, 1, 0);
+
+let session = new TimeSession(1, project, 'some summary', 10, new Date(), new Date(), new Date());
+
+console.log(session);
