c591833b024a45f0c5abfdf8daa4b501dacbe1bc
Jamie Sykes <jamie.sykes@creode.co.uk>
2019-02-28 14:05:01 +0000

    Demonstrates the converttoxml function.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import codebase, { TimeSession, Project, Ticket, User } from 'codebase-npm';
 
-let codebaseConnection = new codebase('', '', '');
+let codebaseConnection = new codebase('<hidden>', '<hidden>', '');
 let project = new Project(1, 'project', 'active', '/project', 1, 1, 0);
 
 let session = new TimeSession(1, project, 'some summary', 10, new Date(), new Date(), new Date());
 
-console.log(session);
+console.log(session.convertToXml());
